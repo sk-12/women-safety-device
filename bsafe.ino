f874bb7eb43ebd588c733dac9a4fb79d030031d2
sk-12 <43299726+sk-12@users.noreply.github.com>
2020-08-06 18:01:42 +0530

    Add files via upload

---
+++ bsafe.ino
@@ -0,0 +1,18 @@
+const int fsensor=A0,buzzer_pin=11;
+void setup() {
+  
+Serial.begin(9600);
+pinMode(fsensor,INPUT);
+pinMode(buzzer_pin,OUTPUT);
+}
+
+void loop() {
+  int flsensor=analogRead(fsensor);
+  
+  if (flsensor>890)
+  {
+    tone(buzzer_pin,1000,500);//buzzer will start playing at a frequency of 1000hz for 500 ms 
+  }
+  
+
+}
